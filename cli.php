258fc668d9fae380efda774c84030d7437b16eff
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2016-08-18 11:31:02 +0200

    feat: storage:redshift-deep-copy command

--- cli.php
+++ cli.php
@@ -5,8 +5,10 @@
 require __DIR__.'/vendor/autoload.php';
 
 use Keboola\Console\Command\MassDedup;
+use Keboola\Console\Command\RedshiftDeepCopy;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
 $application->add(new MassDedup());
+$application->add(new RedshiftDeepCopy());
 $application->run();
